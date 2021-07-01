4104c3fdb021b164fa09eeda90bd8582dfcb202d
Valeriy Emelyanov <emelva@rambler.ru>
2021-07-01 21:44:17 +0300

    Fixing datetime-local

--- src/main/java/com/example/cityexplorer/config/MvcConfig.java
+++ src/main/java/com/example/cityexplorer/config/MvcConfig.java
@@ -3,11 +3,16 @@ package com.example.cityexplorer.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.expression.ParseException;
+import org.springframework.format.Formatter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
@@ -23,5 +28,20 @@ public class MvcConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/img/**")
                 .addResourceLocations("file:/" + uploadPath + "/");
+    }
+
+    @Bean
+    public Formatter<LocalDateTime> localDateTimeFormatter() {
+        return new Formatter<>() {
+            @Override
+            public LocalDateTime parse(String text, Locale locale) throws ParseException {
+                return LocalDateTime.parse(text, DateTimeFormatter.ISO_LOCAL_DATE_TIME);
+            }
+
+            @Override
+            public String print(LocalDateTime object, Locale locale) {
+                return DateTimeFormatter.ISO_LOCAL_DATE_TIME.format(object);
+            }
+        };
     }
 }
